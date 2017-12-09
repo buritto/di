545552b097b32df53cb4a9bf656f5df2f57ea264
buritto <ybiz177@gmail.com>
2017-12-09 16:28:13 +0500

    added class ContentConfigurator

---
+++ TagCloud/ContentConfigurator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagCloud
+{
+    class ContentConfigurator
+    {
+        private readonly HashSet<string> boringWords;
+
+        public ContentConfigurator(HashSet<string> boringWords)
+        {
+            this.boringWords = boringWords;
+        }
+
+        public ContentConfigurator AddBoringWord(string word)
+        {
+            boringWords.Add(word);
+            return this;
+        }
+    }
+}
