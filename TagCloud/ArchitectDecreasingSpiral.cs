63f8f40690dfdea05e4aa2384a1c9230b72e216f
buritto <ybiz177@gmail.com>
2017-12-09 18:29:14 +0500

    added realization of ArchitectDecreasingSpiral class

--- TagCloud/ArchitectDecreasingSpiral.cs
+++ TagCloud/ArchitectDecreasingSpiral.cs
@@ -9,9 +9,19 @@ namespace TagCloud
 {
     class ArchitectDecreasingSpiral : IArchitect
     {
-        public List<Size> GetSizeWords(List<Tuple<string, int>> wordsAndCounts, int heightWindow, int widthWindow)
+        public List<Size> GetSizeWords(List<Tuple<string, int>> wordsAndCounts, int widthWindow, int heightWindow)
         {
-            throw new NotImplementedException();
+            var sizes = new List<Size>(wordsAndCounts.Count);
+            var countWords = wordsAndCounts.Count; 
+            var countAllWords = wordsAndCounts.Sum(tuple => tuple.Item2);
+            return wordsAndCounts.Select(tuple => new Size(
+                GetSide(tuple.Item2, countAllWords, widthWindow, countWords),
+                GetSide(tuple.Item2, countAllWords, heightWindow, countWords))).ToList();
+        }
+
+        private int GetSide(int countWord, int countAllWords, int widthWindow, int countDifferentWords)
+        {
+            return (countWord / countAllWords) * (widthWindow / countDifferentWords);
         }
     }
 }
