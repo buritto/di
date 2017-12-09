06c3c91a94014dfcd85578aafbc1d44a800f4a3f
buritto <ybiz177@gmail.com>
2017-12-09 15:19:24 +0500

    added realization of TxtReader

--- TagCloud/TxtReader.cs
+++ TagCloud/TxtReader.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TagCloud
 {
     class TxtReader : IFormatReader
     {
+        private string formatFile = ".txt";
+
+        public List<string> GetFileData(string fileName)
+        {
+            var wordsFromFile = new List<string>();
+            if (!fileName.EndsWith(formatFile))
+                throw new Exception("Incorrect format file");
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    var line = sr.ReadLine().Split(' ').Where(word => !string.IsNullOrWhiteSpace(word));
+                    wordsFromFile = wordsFromFile.Concat(line).ToList();
+                }
+            }
+
+            return wordsFromFile;
+        }
     }
 }
