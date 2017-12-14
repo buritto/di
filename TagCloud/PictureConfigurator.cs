03f969a3d8bf1ed2ceca49347fb5ba8bd4addfdb
buritto <ybiz177@gmail.com>
2017-12-14 13:07:03 +0500

    rendered check argumenst in method and added class for tests

--- TagCloud/PictureConfigurator.cs
+++ TagCloud/PictureConfigurator.cs
@@ -10,12 +10,21 @@ namespace TagCloud
         public IWordPainter Painter;
 
 
-        public PictureConfigurator(int width, int height, Color color, float maxSize = 120, FontStyle fontStyle = FontStyle.Regular)
+        private void CheckCorrectArgumentConstructor(int width, int height, Color color)
         {
             if (width <= 0 || height <= 0)
             {
                 throw new ArgumentException("Width or height less zero");
             }
+            if (color == null)
+            {
+                throw new ArgumentException("Color is null");
+            }
+        }
+
+        public PictureConfigurator(int width, int height, Color color, float maxSize = 120, FontStyle fontStyle = FontStyle.Regular)
+        {
+            CheckCorrectArgumentConstructor(width, height,color);
             Width = width;
             Height = height;
             Painter = new DefaultPainterWords(color, maxSize, fontStyle);
---
+++ TagCloud/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+using Moq;
+using NUnit.Framework;
+
+namespace TagCloud
+{
+    [TestFixture]
+    class TestPictureConfigurator
+    {
+        [Test]
+        public void CreatePictureConfigurator(int width, int height, Color color)
+        {
+            
+        }
+    }
+}
