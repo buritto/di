588442ba4cb0442173c64b2327ed7a0720a00b9d
buritto <ybiz177@gmail.com>
2017-12-21 21:18:00 +0500

    Added result pattern in TxtReader.cs

--- TagCloud/TxtReader.cs
+++ TagCloud/TxtReader.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using JetBrains.Annotations;
 
 namespace TagCloud
 {
@@ -10,30 +9,48 @@ namespace TagCloud
     {
         private const string formatFile = ".txt";
 
-        [AssertionMethod]
-        private string CheckCorrectFile(string fileName)
+        private Result<IEnumerable<string>> RemoveSpaces(IEnumerable<string> data)
+        {
+            return data.SelectMany(words => words.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries)).AsResult();
+        }
+
+        private Result<IEnumerable<string>> RemovePunctuationMarks(IEnumerable<string> data)
+        {
+            return data.Select(w => w.TrimEnd(',', '.', '?', '!')).AsResult();
+        }
+
+        private Result<IEnumerable<IGrouping<string, string>>> Group(IEnumerable<string> data)
+        {
+            return data.GroupBy(w => w).AsResult();
+        }
+
+        private Result<List<Word>> CreateListPairs(IEnumerable<IGrouping<string, string>> data)
+        {
+            return data.Select(g => new Word
+            {
+                Text = g.Key,
+                Quantity = g.Count()
+            }).ToList().AsResult();
+        }
+
+        private Result<IEnumerable<string>> ReadDataFromFile(string fileName)
         {
             if (!fileName.EndsWith(formatFile))
-               return $"Incorrect file format {fileName}";
+                Result.Fail<IEnumerable<string>>($"Incorrect file format {fileName}");
 
             if (!File.Exists(fileName))
-                return  $"Not found file: {fileName}";
-            return null;
+                Result.Fail<IEnumerable<string>>($"Not found file: {fileName}");
+
+            return File.ReadAllLines(fileName).AsEnumerable().AsResult();
         }
 
         public Result<List<Word>> GetFileData(string fileName)
         {
-            CheckCorrectFile(fileName);
-            return  Result.Of(() => File.ReadLines(fileName)
-                .SelectMany(l => l.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries))
-                .Select(w => w.TrimEnd(',', '.', '?', '!'))
-                .GroupBy(w => w)
-                .Select(g => new Word
-                {
-                    Text = g.Key,
-                    Quantity = g.Count()
-                })
-                .ToList(), CheckCorrectFile(fileName));
+            return ReadDataFromFile(fileName)
+                .Then(RemoveSpaces)
+                .Then(RemovePunctuationMarks)
+                .Then(Group)
+                .Then(CreateListPairs);
         }
     }
 }
