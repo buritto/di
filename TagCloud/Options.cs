30f938a30e04f4412fea4feac02b38a7b581c790
buritto <ybiz177@gmail.com>
2017-12-22 13:31:54 +0500

    render parsing in Options.cs

---
+++ TagCloud/Options.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocoptNet;
+
+namespace TagCloud
+{
+    internal class Options
+    {
+        public int Width { get; }
+        public int Height { get; }
+        public int Count { get; }
+        public float MaxSizeWord { get; }
+        public Color Color { get; }
+        public FontStyle FontStyle { get; }
+        public string TextFileName { get; }
+        public string FileNameWithPicture { get; }
+        private const string usage = @" Tag Cloud
+
+        Usage:
+        TagCloud.exe [--width WIDTH] [--height HEIGHT] [--count COUNT] [--color COLOR] [--style STYLE] [--text TEXT] [--pict PICTURE] [--msize MAX]
+        TagCloud.exe (-h|--help)
+
+        Options:
+        -h --help           Show this screen.
+        --width WIDTH       Width window.[default: 800]
+        --height HEIGHT     Height window.[default: 600]
+        --count COUNT       Minimum number of characters allowed.[default: 3]
+        --msize MAX         The maximum word size in the cloud.[default: 100]
+        --color COLOR       Color text.[default: Red]
+        --style STYLE       Font Style.[default: FontStyle.Regular]
+        --text TEXT         File name\path is have some text.[default: input.txt]
+        --pict PICTURE      File name\path where save picture.[default: output.png]
+    ";
+
+        public Options(string[] args)
+        {
+            var arguments = new Docopt().Apply(usage, args, version: "Tag Cloud 0.1", exit: true);
+       
+            Width = int.Parse(arguments["--width"].Value.ToString());
+            Height = int.Parse(arguments["--height"].Value.ToString());
+            Count = int.Parse(arguments["--count"].Value.ToString());
+            MaxSizeWord = float.Parse(arguments["--msize"].Value.ToString());
+            Color = (Color)new ColorConverter().ConvertFromString(arguments["--color"].ToString());
+            FontStyle = ((Font)new FontConverter().ConvertFromString(arguments["--style"].ToString())).Style;
+            TextFileName = arguments["--text"].Value.ToString();
+            FileNameWithPicture = arguments["--pict"].ToString();
+        }
+    }
+}
--- TagCloud/Program.cs
+++ TagCloud/Program.cs
@@ -5,66 +5,40 @@ using DocoptNet;
 
 namespace TagCloud
 {
+
     public static class Program
     {
-        private static void StartTagCloud(int width, int height, int count,
-            Color color, float maxSizeWord, FontStyle style,
-            string textFileName, string fileNameWithPicture)
+        private static IContainer CreateContainer(Options options)
         {
             var builder = new ContainerBuilder();
             builder.RegisterType<TxtReader>().As<IFormatReader>();
             builder.Register(c =>
             {
                 IWordFilter config = new ContentConfigurator();
-                config = config.SetMinCountSymbolInWord(count);
+                config = config.SetMinCountSymbolInWord(options.Count);
                 return (ContentConfigurator)config;
             }).As<IWordFilter>();
-            builder.Register(c => new PictureConfigurator(width, height, color, maxSizeWord, style)).As<IPainter>();
-            builder.Register(c => new SpiralBuilder(new Point(width / 2, height / 2), width, height))
+            builder.Register(c => new PictureConfigurator(options.Width, options.Height,
+                options.Color, options.MaxSizeWord, options.FontStyle)).As<IPainter>();
+            var centerWindow = new Point(options.Width / 2, options.Height / 2);
+            builder.Register(c => new SpiralBuilder(centerWindow, options.Width, options.Height))
                 .As<ITagCloudBuilder>();
             builder.RegisterType<TagCloud>();
-            var container = builder.Build();
-            using (container.BeginLifetimeScope())
-            {
-                var component = container.Resolve<TagCloud>();
-                var picture = component.PaintTagCloud(textFileName, fileNameWithPicture);
-                picture.EnsureSuccess();
-            }
+             return builder.Build();
         }
 
-        private const string usage = @" Tag Cloud
-
-        Usage:
-        TagCloud.exe [--width WIDTH] [--height HEIGHT] [--count COUNT] [--color COLOR] [--style STYLE] [--text TEXT] [--pict PICTURE] [--msize MAX]
-        TagCloud.exe (-h|--help)
-
-        Options:
-        -h --help           Show this screen.
-        --width WIDTH       Width window.[default: 800]
-        --height HEIGHT     Height window.[default: 600]
-        --count COUNT       Minimum number of characters allowed.[default: 3]
-        --msize MAX         The maximum word size in the cloud.[default: 100]
-        --color COLOR       Color text.[default: Red]
-        --style STYLE       Font Style.[default: FontStyle.Regular]
-        --text TEXT         File name\path is have some text.[default: input.txt]
-        --pict PICTURE      File name\path where save picture.[default: output.png]
-    ";
+        
 
         static void Main(string[] args)
         {
             try
             {
-                var arguments = new Docopt().Apply(usage, args, version: "Tag Cloud 0.1", exit: true);
+                var options = new Options(args);
+                CreateContainer(options)
+                    .Resolve<TagCloud>()
+                    .PaintTagCloud(options.TextFileName, options.FileNameWithPicture)
+                    .EnsureSuccess();
 
-                var width = int.Parse(arguments["--width"].Value.ToString());
-                var height = int.Parse(arguments["--height"].Value.ToString());
-                var count = int.Parse(arguments["--count"].Value.ToString());
-                var maxSizeWord = float.Parse(arguments["--msize"].Value.ToString());
-                var color = (Color) new ColorConverter().ConvertFromString(arguments["--color"].ToString());
-                var fontStyle =  ((Font) new FontConverter().ConvertFromString(arguments["--style"].ToString())).Style;
-                var textFileName = arguments["--text"].Value.ToString();
-                var fileNameWithPicture = arguments["--pict"].ToString();
-                StartTagCloud(width, height, count, color, maxSizeWord, fontStyle, textFileName, fileNameWithPicture);
             }
             catch (Exception e)
             {
